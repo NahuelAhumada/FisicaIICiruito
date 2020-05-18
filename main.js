0f93736bfba9298131a69d691704930f5869dbd4
Nahuel Ahumada <ahumada.nahuel35149@gmail.com>
2020-05-18 15:14:05 -0300

    clases

---
+++ main.js
@@ -0,0 +1,17 @@
+class Capacitor{
+    constructor(carga, voltaje, capacitancia){
+        this.carga=carga;
+        this.voltaje=voltaje;
+        this.capacitancia=capacitancia;
+    }
+}
+class Resistencia{
+    constructor(ohms){
+        this.ohms=ohms;
+    }
+}
+class Fem{
+    constructor(voltaje){
+        this.voltaje=voltaje;
+    }
+}
